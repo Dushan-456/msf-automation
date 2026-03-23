6b1fd305621647224e6b3fc4ea111d61bc7a3f88
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2026-03-23 17:31:23 +0530

    Update emailService.mjs

--- Backend/src/services/emailService.mjs
+++ Backend/src/services/emailService.mjs
@@ -24,10 +24,16 @@ export const sendDoctorNotificationEmail = async (doctorName, doctorEmail) => {
       html: `
             <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
                 <p>Dear ${doctorName},</p>
-                <p>We are pleased to inform you that your Multi-Source Feedback (MSF) survey has been successfully <br>distributed to your selected feedback providers via email.</p>
+                <p>We are pleased to inform you that your Multi-Source Feedback (MSF) survey has been successfully <br>
+                distributed to your selected feedback providers via email.</p>
+
                 <p>The analysis will be carried out only after receiving a <strong>minimum of 12 responses</strong> from the feedback providers.</p>
-                <p>To help ensure a high response rate, we kindly request that you remind your feedback providers to <br> complete and submit their responses as soon as possible. Please also inform them that the survey email may <br>sometimes be delivered to the spam/junk folder, and to check those folders as well.</p>
-                <p>We also kindly request that you follow up on your MSF status and contact MERC <a href="mailto:merc@pgim.cmb.ac.lk">merc@pgim.cmb.ac.lk</a> if you require any <br>clarification.</p>
+
+                <p>To help ensure a high response rate, we kindly request that you remind your feedback providers to <br> 
+                complete and submit their responses as soon as possible. If any responders indicate that they have not received the email, please inform <br>
+                them that the survey email may sometimes be delivered to the spam/junk folder and advise them to check those folders as well.</p>
+
+                <p>We also kindly request that you follow up on your MSF status and contact MERC <a href="mailto:merc@pgim.cmb.ac.lk">merc@pgim.cmb.ac.lk</a> if you require any clarification.</p>
                 <p>Thank you for your cooperation.</p>
                 <br>
                 <p>Best regards,</p>
